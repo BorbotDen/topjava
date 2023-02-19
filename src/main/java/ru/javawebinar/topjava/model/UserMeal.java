c7a6923e50d2d24fb444061e5a5850c19d5acab0
Denis Borbot <Borbot@mail.ru>
2023-02-19 16:43:24 +0400

    Make solution with streams

--- src/main/java/ru/javawebinar/topjava/model/UserMeal.java
+++ src/main/java/ru/javawebinar/topjava/model/UserMeal.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.topjava.model;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class UserMeal {
@@ -17,8 +18,13 @@ public class UserMeal {
 
     public LocalDateTime getDateTime() {
         return dateTime;
+    } 
+    public LocalDate getDate() {
+        return LocalDate.from(dateTime);
     }
-
+    public LocalDate getTime() {
+        return LocalDate.from(dateTime);
+    }
     public String getDescription() {
         return description;
     }
